91e89d0721c9a578b819a953831d0469ac1903fd
Kory <kory.tashim@icloud.com>
2025-11-08 03:17:04 +0500

    working with nav

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+    return(
+        <div className="nav">
+            <img src="Gemini_Generated_Image_fhdy3rfhdy3rfhdy2.png" height={150}/>
+
+            <div className="navbar">
+                <p>HOME</p>
+                <p>GAME</p>
+                <p>NEWS</p>
+                <p>CONTACT</p>
+            </div>
+        </div>
+    )
+}
+
+export default Header
