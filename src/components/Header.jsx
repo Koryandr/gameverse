ccd684d6a41be338bd06eff3b11760373b37ce19
Kory <kory.tashim@icloud.com>
2025-11-08 18:14:00 +0500

    почти доделал head

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,31 @@
 const Header = () => {
     return(
-        <div className="nav">
-            <img src="Gemini_Generated_Image_fhdy3rfhdy3rfhdy2.png" height={150}/>
-
-            <div className="navbar">
-                <p>HOME</p>
-                <p>GAME</p>
-                <p>NEWS</p>
-                <p>CONTACT</p>
+        <div>
+            <div className="nav">
+            <img src="Gemini_Generated_Image_fhdy3rfhdy3rfhdy2.png" height={90}/>
+                <div className="navbar">
+                    <p>HOME</p>
+                    <p>GAME</p>
+                    <p>NEWS</p>
+                    <p>CONTACT</p>
+                </div>
             </div>
+
+            <div className="hero-section"> 
+                <div className="content-container"> 
+                    <div className="main-text">
+                        <p>SPIDERMAN</p>
+                        <p>GAME-SHOP</p>
+                    </div>
+                    <div className="small-text">
+                        <p>Welcome to GameVerse a place where every player becomes<br/>the hero of their own story. Here you'll find the best titles, exclusive<br/>new releases, and legendary hits, all gathered in one place.</p>
+                    </div>
+                    <button className="shop-button">START SHOPPING</button>
+                </div>
+
+                <img src="17626047240802.png" className="hero-image" width={800}/>
+            </div> 
         </div>
     )
 }
-
 export default Header
