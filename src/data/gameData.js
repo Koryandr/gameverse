c8bada9911af4f1f885edcabe1fbca7f4dde56b8
Kory <kory.tashim@icloud.com>
2025-11-10 08:53:40 +0500

    изменил css колон и карточек

--- src/data/gameData.js
+++ src/data/gameData.js
@@ -2,26 +2,26 @@
 
 // --- Колонка "New Release" (4 игры) ---
 const newRelease = [
-    { id: 1, title: 'Lego StarWars Skywalker Saga', status: '$59.99', image: '/images/lego.jpg' },
-    { id: 2, title: 'Ghost Wire Tokyo', status: '$30.00', image: '/images/ghostwire.jpg' },
-    { id: 3, title: 'SIFU', status: '$29.00', image: '/images/sifu.jpg' },
-    { id: 4, title: 'Horizon: Forbidden West', status: '$65.00', image: '/images/horizon.jpg' },
+    { id: 1, title: 'Lego StarWars Skywalker Saga', status: '$59.99', image: '/image/image 4.png' },
+    { id: 2, title: 'Ghost Wire Tokyo', status: '$30.00', image: '/image/image 5.png' },
+    { id: 3, title: 'SIFU', status: '$29.00', image: '/image/image 6.png' },
+    { id: 4, title: 'Horizon: Forbidden West', status: '$65.00', image: '/image/image 7.png' },
 ];
 
 // --- Колонка "Free Games" (4 игры) ---
 const freeGames = [
-    { id: 5, title: 'Dota 2', status: 'Free', image: '/images/dota.jpg' },
-    { id: 6, title: 'Fortnite', status: 'Free', image: '/images/fortnite.jpg' },
-    { id: 7, title: 'Apex Legends', status: 'Free', image: '/images/apex.jpg' },
-    { id: 8, title: 'Valorant', status: 'Free', image: '/images/valorant.jpg' },
+    { id: 5, title: 'Dota 2', status: 'Free', image: '/image/image 8.png' },
+    { id: 6, title: 'Fortnite', status: 'Free', image: '/image/image 9.png' },
+    { id: 7, title: 'Apex Legends', status: 'Free', image: '/image/image 10.png' },
+    { id: 8, title: 'Valorant', status: 'Free', image: '/image/image 11.png' },
 ];
 
 // --- Колонка "Coming Soon" (4 игры) ---
 const comingSoon = [
-    { id: 9, title: 'Evil Dead The Game', status: 'Coming Soon', image: '/images/evildead.jpg' },
-    { id: 10, title: 'Gotham Knights', status: 'Coming Soon', image: '/images/gotham.jpg' },
-    { id: 11, title: 'Hogwarts Legacy', status: 'Coming Soon', image: '/images/hogwarts.jpg' },
-    { id: 12, title: 'God of War: RAGNAROK', status: 'Coming Soon', image: '/images/gow.jpg' },
+    { id: 9, title: 'Evil Dead The Game', status: 'Coming Soon', image: '/image/image 12.png' },
+    { id: 10, title: 'Gotham Knights', status: 'Coming Soon', image: '/image/image 13.png' },
+    { id: 11, title: 'Hogwarts Legacy', status: 'Coming Soon', image: '/image/image 14.png' },
+    { id: 12, title: 'God of War: RAGNAROK', status: 'Coming Soon', image: '/image/image 15.png' },
 ];
 
 // ВАЖНО: Экспортируем все три массива!
