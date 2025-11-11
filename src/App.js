e10bea66feb3673db4531f3f5fe42f88754998b1
Kory <kory.tashim@icloud.com>
2025-11-11 09:33:33 +0500

    работаю над news разделом

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-// src/App.js
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Main from './components/Main'; 
 import GameShowcase from './components/GameShowcase';
 import BrowseGamesSection from './components/BrowseGamesSection';
+import News from './components/News';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Main/> 
-      <GameShowcase /> 
       <BrowseGamesSection/>
+      <GameShowcase /> 
+      <News/>
     </div>
   );
 }
---
+++ src/components/News.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Search } from "lucide-react"; // ✅ добавь эту строку
+import "./News.css";
+
+const News = () => {
+  return (
+    <>
+      <div className="news-nav">
+        <img src="1300388.png" width={50} alt="logo" />
+        <p className="games-text">ONLY THE BEST NEWS</p>
+        <img src="1300388.png" width={50} alt="logo" />
+      </div>
+    <div className="navbar-news">
+      <p className="news-main">News</p>
+
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search"
+          className="search-input"
+        />
+        <Search className="search-icon" />
+      </div>
+    </div>
+    
+    <div className="new-game">
+        <img src="horizen.png"/>
+        <img src="callofduty.png"/>
+        <img src="gta.png"/>
+        <img src="lara.png"/>
+    </div>
+
+    <p className="tournaments">Popular Tournaments</p>
+    </>
+  );
+};
+
+export default News;
