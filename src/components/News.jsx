16ce2acc3f7170e648cdffba533b27457e5e5ccc
Kory <kory.tashim@icloud.com>
2025-11-13 05:35:39 +0500

    добавил tournaments раздел

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -10,27 +10,44 @@ const News = () => {
         <p className="games-text">ONLY THE BEST NEWS</p>
         <img src="1300388.png" width={50} alt="logo" />
       </div>
-    <div className="navbar-news">
-      <p className="news-main">News</p>
 
-      <div className="search-bar">
-        <input
-          type="text"
-          placeholder="Search"
-          className="search-input"
-        />
-        <Search className="search-icon" />
+      <div className="navbar-news">
+        <p className="news-main">News</p>
+
+        <div className="search-bar">
+          <input
+            type="text"
+            placeholder="Search"
+            className="search-input"
+          />
+          <Search className="search-icon" />
+        </div>
       </div>
-    </div>
-    
-    <div className="new-game">
-        <img src="horizen.png"/>
-        <img src="callofduty.png"/>
-        <img src="gta.png"/>
-        <img src="lara.png"/>
-    </div>
 
-    <p className="tournaments">Popular Tournaments</p>
+      <div className="new-game">
+        <img src="horizen.png" alt="Horizon" />
+        <img src="callofduty.png" alt="Call of Duty" />
+        <img src="gta.png" alt="GTA VI" />
+        <img src="lara.png" alt="Lara Croft" />
+      </div>
+
+      <p className="tournaments">Popular Tournaments</p>
+
+      <div className="Tournaments-news">
+        {[
+          "PopularTournamentCard1.png",
+          "PopularTournamentCard2.png",
+          "PopularTournamentCard3.png",
+          "PopularTournamentCard4.png",
+        ].map((src, i) => (
+          <div key={i} className="tournament-card">
+            <img src={src} alt={`tournament-${i}`} />
+            <button className="light-button">
+              <img src="Light.png" alt="light" />
+            </button>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
